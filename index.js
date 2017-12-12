dc84e9888943321e95f2b5c8736f1b98f9d4cba7
Stephen Boynton <boyntonsc@gmail.com>
2017-12-12 13:26:49 -0500

    New component

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { AppRegistry, Text } from "react-native";
+
+const App = () => {
+  return <Text>Some Text</Text>;
+};
+
+AppRegistry.registerComponent("albums", () => App);
